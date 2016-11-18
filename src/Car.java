1c307ca5f82f825803952a50bf68c47c258af645
Thomas Phillips <thomasphillips3@gmail.com>
2016-11-17 21:21:04 -0800

    Added some components.

--- src/Car.java
+++ src/Car.java
@@ -1,8 +1,15 @@
+import components.Body;
+import components.Engine;
+import components.Transmission;
 
 /**
  * Created by thomas on 11/13/16.
  */
 public class Car {
+    Engine engine;
+    Transmission transmission;
+    Body body;
+
     public static void main (String[] args) {
     }
 }
