41afbf9548d358de1dacae28eeda02cb3a3c604e
Thomas Phillips <thomasphillips3@gmail.com>
2016-11-17 21:19:04 -0800

    Added enum for transmission type.

--- src/components/Transmission.java
+++ src/components/Transmission.java
@@ -4,4 +4,11 @@ package components;
  * Created by thomas on 11/13/16.
  */
 public class Transmission {
+    enum Type {
+        MANUAL,
+        AUTOMATIC,
+        CONTINUOUSLY_VARIABLE,
+        DUAL_CLUTCH,
+        HYBRID
+    }
 }
