55f3249478a2f3383391c4da73fbf70d5c5a9e47
Thomas Phillips <thomasphillips3@gmail.com>
2016-11-17 19:54:13 -0800

    Cleaned up the tests junk from Car.java.

--- src/Car.java
+++ src/Car.java
@@ -1,18 +1,8 @@
-import components.Engine;
-import components.Transmission;
 
 /**
  * Created by thomas on 11/13/16.
  */
 public class Car {
-    Car() {
-        int wheels = 4;
-        int currentSpeed;
-        int horsepower;
-
-        Engine engine = new Engine();
-        Transmission transmission = new Transmission();
-    }
     public static void main (String[] args) {
     }
 }
